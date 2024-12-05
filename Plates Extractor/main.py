c9c28cd8f57e22fcc1d693654aff1307b95af7a2
Bishoywadea <bishoyw.fathy@gmail.com>
2024-12-05 15:30:06 +0200

    enhanced the accuracy

--- Plates Extractor/main.py
+++ Plates Extractor/main.py
@@ -1,23 +1,23 @@
 from multiprocessing import Process, Queue
 from sampler import sample_video
 from extractor import image_consumer
-
+import cv2
+import skimage
+import os
 def main():
     image_queue = Queue()
 
-    producer_process = Process(target=sample_video, args=(image_queue, "dataset/video.mp4"))
+    producer_process = Process(target=sample_video, args=(image_queue, "dataset/02.mp4"))
     producer_process.start()
 
     consumer_processes = []
-    for i in range(4):  # Create 4 consumers
+    for i in range(10):  
         consumer_process = Process(target=image_consumer, args=(image_queue, i+1))
         consumer_processes.append(consumer_process)
         consumer_process.start()
 
-    # Wait for the producer to finish
     producer_process.join()
 
-    # Wait for all consumer processes to finish
     for consumer_process in consumer_processes:
         consumer_process.join()
 
