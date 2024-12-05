ca6e6cbf62c9bc4dc0342eee6220d3dda1e4524f
Bishoywadea <bishoyw.fathy@gmail.com>
2024-12-05 15:30:06 +0200

    enhanced the accuracy

--- Plates Extractor/extractor.py
+++ Plates Extractor/extractor.py
@@ -8,42 +8,53 @@ def save_image(image, filename):
     cv2.imwrite(filename, image)
 
 def process_frame(frame, frame_count, index):
+    frame = frame[50:, :]
+
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    smoothed_image = cv2.bilateralFilter(gray, 3, 40, 40)
-    edged_image = cv2.Canny(smoothed_image, 85, 200)
+    smoothed_image = cv2.bilateralFilter(gray, 25, 95, 95)
 
-    keypoints = cv2.findContours(edged_image.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    edged_image = cv2.Canny(smoothed_image, 80, 190)
+
+
+    kernel = np.ones((1, 20), np.uint8)
+    dilated_image = cv2.dilate(edged_image, kernel, iterations=1)
+
+    eroded_image = cv2.erode(dilated_image, kernel, iterations=1)
+
+
+    keypoints = cv2.findContours(eroded_image.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     contours = imutils.grab_contours(keypoints)
-    contours = sorted(contours, key=cv2.contourArea, reverse=True)[:10]
+    contours = sorted(contours, key=cv2.contourArea, reverse=True)
 
-    location = None
+    location = []
     for contour in contours:
         approx = cv2.approxPolyDP(contour, 10, True)
         if len(approx) == 4:
-            location = approx
-            break
+            location.append(approx)
 
-    if location is None:
+    if not location:
         print("No rectangles found in the frame.")
         return
 
-    mask = np.zeros(gray.shape, np.uint8)
-    isolated_plate = cv2.drawContours(mask, [location], 0, 255, -1)
-    isolated_plate = cv2.bitwise_and(frame, frame, mask=mask)
+    isolated_plate  = np.zeros_like(frame)
 
-    (x, y) = np.where(mask == 255)
-    (x1, y1) = (np.min(x), np.min(y))
-    (x2, y2) = (np.max(x), np.max(y))
-    cropped_image = gray[x1:x2 + 1, y1:y2 + 1]
+    for idx, loc in enumerate(location):
+        mask = np.zeros_like(gray)
+        cv2.drawContours(mask, [loc], -1, 255, thickness=cv2.FILLED)
 
-    # Save the images with unique filenames
+        isolated_plate[mask == 255] = frame[mask == 255]
+
+    isolated_plate_gray = cv2.cvtColor(isolated_plate, cv2.COLOR_BGR2GRAY)
+
     output_dir = "processed_images"
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
-    save_image(gray, os.path.join(output_dir, f"frame_{index}_{frame_count}_gray.jpg"))
-    save_image(cropped_image, os.path.join(output_dir, f"frame_{index}_{frame_count}_cropped.jpg"))
+    save_image(frame, os.path.join(output_dir, f"frame_{index}_{frame_count}_original.jpg"))
+    save_image(edged_image, os.path.join(output_dir, f"frame_{index}_{frame_count}edged.jpg"))
+    save_image(isolated_plate_gray, os.path.join(output_dir, f"frame_{index}_{frame_count}_isolated_plate_gray.jpg"))
+    save_image(dilated_image, os.path.join(output_dir, f"frame_{index}_{frame_count}_dilated.jpg"))
 
     print(f"Saved images for frame {frame_count}")
 
@@ -61,7 +72,7 @@ def image_consumer(image_queue, index):
             print(f"Processing frame {frame_count}...")
             process_frame(frame, frame_count,index)
             print(f"Frame {frame_count} processed and saved.")
-        except image_queue.Empty:
+        except image_queue.empty:
             print("Queue is empty, waiting for more frames.")
         except Exception as e:
             print(f"Error processing frame: {e}")
